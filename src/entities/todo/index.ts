a70fc8358eb6c11163943d3dfc59acf04c26c223
Artem Antonov <onlyformywork0@gmail.com>
2023-08-05 13:56:55 -0400

    Added Todo entity

---
+++ src/entities/todo/index.ts
@@ -0,0 +1,9 @@
+export {
+  useGetTodosQuery,
+  useGetTodoByIdQuery,
+  useCreateTodoMutation,
+  useDeleteTodoMutation,
+  useUpdateTodoTitleMutation,
+  useUpdateTodoStatusMutation,
+} from './api/todoApi';
+export { TodoCard } from './ui/todo-card';
--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,7 +1,9 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { baseQuery } from './baseQuery';
+import { TODO_TAG } from './tags';
 
 export const baseApi = createApi({
+  tagTypes: [TODO_TAG],
   reducerPath: 'api',
   baseQuery,
   endpoints: () => ({}),
